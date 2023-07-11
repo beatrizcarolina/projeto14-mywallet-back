ba7b48c7eb9de62cf38281df5d2427e92fd12742
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-11 15:00:24 -0300

    fix: signOut function

--- src/middlewares/authValidation.js
+++ src/middlewares/authValidation.js
@@ -1,8 +1,10 @@
 import { db } from "../database/database.js";
 
 export async function authentication(req, res, next) {
-    const { auth } = req.headers;
-    const token = auth?.replace("Bearer ", "");
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    console.log("Validate Token: " + token);
 
     if (!token) {
         return res.sendStatus(401);
@@ -10,6 +12,7 @@ export async function authentication(req, res, next) {
 
     try {
         const session = await db.collection("sessions").findOne({ token });
+        console.log("Session: " + session);
 
         if (!session) {
             return res.sendStatus(401);
