8fd4208f26422b6859603c10fdc0b0e8a8eb7f60
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-11 14:44:22 -0300

    configure user router

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRouter from "./auth.router.js";
+import userRouter from "./user.router.js";
 
 const router = Router();
 router.use(authRouter);
+router.use(userRouter);
 
 export default router;
