0e1569963942bba6418bd286d209b1169e3ff563
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-11 15:52:02 -0300

    config /nova-transacao router

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { authentication } from "../middlewares/authValidation.js"
-import { getTransactions } from "../controllers/userController.js";
+import { getTransactions, newTransaction } from "../controllers/userController.js";
+import { schemaValidation } from "../middlewares/schemaValidation.js";
+import { transactionSchema } from "../schemas/userSchema.js";
 
 const userRouter = Router();
 userRouter.get("/home", authentication, getTransactions);
+userRouter.post("/nova-transacao/:tipo", authentication, schemaValidation(transactionSchema), newTransaction);
 
 export default userRouter
