7179fca6d700b737500cf86f578f39190b827351
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-11 14:21:31 -0300

    configure signOut route

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { signUpSchema, signInSchema } from "../schemas/authSchema.js";
-import { signUp, signIn } from "../controllers/authController.js";
+import { signUp, signIn, signOut } from "../controllers/authController.js";
 import { schemaValidation } from "../middlewares/schemaValidation.js"
+import { authentication } from "../middlewares/authValidation.js";
 
 const authRouter = Router();
 authRouter.post("/cadastro", schemaValidation(signUpSchema), signUp);
 authRouter.post("/", schemaValidation(signInSchema), signIn);
+authRouter.delete("/signout", authentication, signOut);
 
 export default authRouter;
